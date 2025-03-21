20d4f3e87ce26eed16a69b55b8b5ce2c4fef0a1a
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2025-03-21 09:53:42 +0100

    feature: commenting main

--- cmd/gofuncor/main.go
+++ cmd/gofuncor/main.go
@@ -3,12 +3,11 @@ package main
 import (
 	"github.com/manuelarte/gofuncor/pkg/analyzer"
 	"golang.org/x/tools/go/analysis"
-	"golang.org/x/tools/go/analysis/singlechecker"
 )
 
-func main() {
-	singlechecker.Main(analyzer.NewAnalyzer())
-}
+//func main() {
+//	singlechecker.Main(analyzer.NewAnalyzer())
+//}
 
 func New(_ any) ([]*analysis.Analyzer, error) {
 	return []*analysis.Analyzer{analyzer.NewAnalyzer()}, nil
