2f6dcab5496e242c4da7e3b0a4ac64dc3a5257c2
Ludovic Fernandez <ldez@users.noreply.github.com>
2025-03-27 13:53:00 +0100

    chore: simplify tests (#13)

--- analyzer/analyzer_test.go
+++ analyzer/analyzer_test.go
@@ -1,21 +1,12 @@
 package analyzer_test
 
 import (
-	"os"
-	"path/filepath"
 	"testing"
 
 	"github.com/manuelarte/funcorder/analyzer"
-
 	"golang.org/x/tools/go/analysis/analysistest"
 )
 
 func TestAll(t *testing.T) {
-	path, err := os.Getwd()
-	if err != nil {
-		t.Fatalf("Failed to get wd: %s", err)
-	}
-
-	testdata := filepath.Join(filepath.Dir(filepath.Dir(path)), "funcorder/testdata")
-	analysistest.Run(t, testdata, analyzer.NewAnalyzer(), "simple")
+	analysistest.Run(t, analysistest.TestData(), analyzer.NewAnalyzer(), "simple")
 }
