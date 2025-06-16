6c1e657e525f80ec34de03b115b001610e37bb32
Manuel Doncel Martos <manuel.doncel.martos@ing.com>
2025-06-16 15:11:32 +0200

    updating test with a real example

--- analyzer/testdata/src/multiple-types/multiple-types.go
+++ analyzer/testdata/src/multiple-types/multiple-types.go
@@ -5,10 +5,18 @@ type (
 	MyStruct2 struct{}
 )
 
-func NewMyStruct() *MyStruct {
+func (MyStruct) hello() string {
+	return "hello"
+}
+
+func (MyStruct2) bye() string {
+	return "bye"
+}
+
+func NewMyStruct() *MyStruct { // want `constructor "NewMyStruct" for struct "MyStruct" should be placed before struct method "hello"`
 	return &MyStruct{}
 }
 
-func NewMyStruct2() *MyStruct2 {
+func NewMyStruct2() *MyStruct2 { // want `constructor "NewMyStruct2" for struct "MyStruct2" should be placed before struct method "bye`
 	return &MyStruct2{}
 }
